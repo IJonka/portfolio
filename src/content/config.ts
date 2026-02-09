2edb6520f2f942bed0939dc198a8ca4c998403dc
IJonka <javohiribr@gmail.com>
2026-02-09 16:01:07 +0500

    fix pagescms

--- src/content/config.ts
+++ src/content/config.ts
@@ -3,9 +3,10 @@ import { defineCollection, z } from "astro:content";
 const heroProjects = defineCollection({
   type: "content",
   schema: z.object({
-    title: z.string(),
     image: z.string(),
-    link: z.string(),
+    title: z.string().optional(), // Добавили optional
+    link: z.string().optional(), // Добавили optional
+    alt: z.string().optional(), // Добавили alt, который был в yaml
   }),
 });
 

4d27e2147e9de74b439f2484268b8e9e736e0316
IJonka <javohiribr@gmail.com>
2026-02-09 16:09:26 +0500

    PAGESCMS

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,12 @@
+/// <reference types="astro/client" />
 import { defineCollection, z } from "astro:content";
 
 const heroProjects = defineCollection({
   type: "content",
   schema: z.object({
+    title: z.string().optional(), // Добавь .optional()
     image: z.string(),
-    title: z.string().optional(), // Добавили optional
-    link: z.string().optional(), // Добавили optional
-    alt: z.string().optional(), // Добавили alt, который был в yaml
+    link: z.string().optional(), // Добавь .optional()
   }),
 });
 
